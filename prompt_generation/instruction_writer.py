2a30e43cecca6eec3e0bd8fec51f6127fe3c58af
Kamran Vatankhah <kamran.vat@protonmail.com>
2023-12-17 14:16:46 +0100

    added generation of dict of all combinations to instruction_writer

--- prompt_generation/instruction_writer.py
+++ prompt_generation/instruction_writer.py
@@ -46,29 +46,30 @@ def generate_shape_list(directory, shape_amount):
     return shapes[:shape_amount]
 
 
-def read_instructions(json_file, instr_type):
-    """Reads a json file with "instr_type: instruction" formatted values. Returns a list of those instructions that are of instr_type
+def read_instructions_by_type(json_file, instr_type):
+    """Reads a json file with "instr_type: instruction" formatted values. Returns a list of dicts of those instructions that are of instr_type
 
     Args:
         json_file (str): path to .json file with instructions
         instr_type (str): the type of instruction to filter and return as a list
 
     Returns:
-        list: all instruction elements of instr_type
-    """    
+        list: list of dicts, all instruction elements of instr_type
+    """
     with open(json_file, "r") as file:
         data = json.load(file)
 
     filtered_instructions = []
+
     for element in data:
         if instr_type in element:
-            filtered_instructions.append(element[instr_type])
+            filtered_instructions.append(element)
 
     return filtered_instructions
 
 
-def generate_instr_list(json_file, instr_type, instr_amount):
-    instructions = read_instructions(json_file, instr_type)
+def generate_instr_list_by_type(json_file, instr_type, instr_amount):
+    instructions = read_instructions_by_type(json_file, instr_type)
     return instructions[instr_amount]
 
 
@@ -77,37 +78,6 @@ def generate_size_modifiers(size_amount, size_modifier_list):
         return []
     else:
         return size_modifier_list[:size_amount]
-
-
-def calculate_total_variations_old(
-    shape_list, *instruction_lists, color_list=None, size_list=None
-):
-    """Multiply the length of all lists to get the amount of total possible variations
-
-    Args:
-        shape_list (list): list of shapes for prompt generation
-        *instruction_lists (list): list of lists, each containing instructions of one type
-        color_list (list): list of colors for prompt generation
-        size_list (list): list of sizes for prompt generation
-
-    Returns:
-        int: Length of all args (if non-zero), multiplied (*instruction_lists handled like a single list)
-    """
-
-    list_lengths = [color_list, shape_list, size_list]
-
-    # Handle instruction lists as one, since each prompt only contains one instruction
-    for list in instruction_lists:
-        if list != None:
-            list_lengths.append(len(list))
-
-    # Multiply all values to get amount of variations, ignoring 0-values
-    result = 1
-    for element in list_lengths:
-        if element != None:
-            if len(element != 0):
-                result *= len(element)
-    return result
 
 
 def calculate_total_variations(
@@ -136,7 +106,7 @@ def calculate_total_variations(
     elif type(instruction_amounts) == int:
         list_lengths.append(instruction_amounts)
     else:
-        raise TypeError #TODO implement errors
+        raise TypeError  # TODO implement errors
 
     # Multiply all non-0 values
     result = 1
@@ -146,33 +116,21 @@ def calculate_total_variations(
     return result
 
 
-def generate_prompt_dict(
-    color_list, shape_list, *instruction_lists, size_list=None
-):
-    prompt_dict = {}
+def generate_prompt_dict(color_list, shape_list, instruction_lists, size_list):
+    instruction_list = [
+        {"type": list(instr_dict.keys())[0], "value": list(instr_dict.values())[0]}
+        for sublist in instruction_lists
+        for instr_dict in sublist
+    ]
 
-    # Create a list containing all combinations of elements from the input lists
-    combinations = calculate_total_variations(
-        color_list, shape_list, *instruction_lists, size_list
-    )
+    prompt_list = [
+        {"color": color, "shape": shape, "instruction": instruction, "size": size}
+        for color, shape, instruction, size in product(
+            color_list, shape_list, instruction_list, size_list
+        )
+    ]
 
-    for combination in combinations:
-        color, shape = combination[:2]
-        instructions = {"color": color, "shape": shape}
-
-        # Add size to the instruction if size_list is provided
-        if size_list:
-            instructions["size"] = size_list[color_list.index(color)]
-
-        # Add additional instructions from the rest of the combination
-        for idx, instruction_list in enumerate(combination[2:]):
-            instructions[f"instruction_list_{idx + 1}"] = instruction_list
-
-        # Create a unique key for each combination
-        key = tuple(combination)
-        prompt_dict[key] = instructions
-
-    return prompt_dict
+    return prompt_list
 
 
 def main():
@@ -186,7 +144,8 @@ def main():
     color_list = read_colors(color_file_path)
     shape_list = read_mujoco_shapes(xml_directory_path)
     instr_lists = [
-        read_instructions(instr_file_path, instr_type) for instr_type in instr_types
+        read_instructions_by_type(instr_file_path, instr_type)
+        for instr_type in instr_types
     ]
 
     # Metadata for CLI
@@ -205,7 +164,7 @@ def main():
         "--color_amount",
         type=int,
         choices=range(1, max_color_amount + 1),
-        default=2,
+        default=3,
         help="Number of colors",
     )
     parser.add_argument(
@@ -223,11 +182,10 @@ def main():
             f"--{attribute_name}",
             type=int,
             choices=range(1, max_instr_amount + 1),
-            default=[1],
+            default=1,
             nargs="+",
             help=f"Instruction amounts for {attribute_name}",
         )
-
 
     parser.add_argument(
         "--size_amount",
@@ -242,7 +200,10 @@ def main():
     # Initial calculation
     instr_amounts = max_instr_amounts
     total_variations = calculate_total_variations(
-        color_amount=args.color_amount, shape_amount=args.shape_amount, instruction_amounts=instr_amounts, size_amount=args.size_amount
+        color_amount=args.color_amount,
+        shape_amount=args.shape_amount,
+        instruction_amounts=instr_amounts,
+        size_amount=args.size_amount,
     )
     print(f"Initial Total Variations: {total_variations}")
 
@@ -273,7 +234,10 @@ def main():
 
         # Calculate total variations
         total_variations = calculate_total_variations(
-            color_amount=color_amount, shape_amount=shape_amount, instruction_amounts=instr_amounts, size_amount=size_amount
+            color_amount=color_amount,
+            shape_amount=shape_amount,
+            instruction_amounts=instr_amounts,
+            size_amount=size_amount,
         )
         print(f"Total Variations: {total_variations}")
 
@@ -283,17 +247,24 @@ def main():
         args.instr_amounts = instr_amounts
         args.size_amount = size_amount
 
-        if input("\nDo you want to continue? (yes/no): ").lower() != "yes":
+        if (
+            input(
+                "\nDo you want to alter these values? (type no to proceed) (yes/no): "
+            ).lower()
+            != "yes"
+        ):
             break
+
+    # TODO consider what goes here: call the "generate " functions to generate lists of the lengths
+    # i want, or just shorten the lists I already have.
+    # also, is the format for the instructions okay (being list instead of list of dicts like all the others?)
 
     combinations = generate_prompt_dict(
         color_list=color_list,
         shape_list=shape_list,
         size_list=size_modifier_list,
-        *instr_lists,
+        instruction_lists=instr_lists,
     )
-
-    print(combinations)
 
 
 if __name__ == "__main__":
